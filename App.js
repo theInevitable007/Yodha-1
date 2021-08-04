e77563d7be925d97c705dd234e7d10d8d82822f7
theInevitable007 <66513833+theInevitable007@users.noreply.github.com>
2021-08-04 19:36:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './screens/WelcomeScreen'
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <WelcomeScreen/>
+    );
+  }
+  }
+  
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
